df1e64649885566c35f35ee3359fa973787b1bf4
Arnold Lee <arnoldlee60@gmail.com>
2021-05-21 15:22:54 -0500

    something broke

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,78 @@
 var startButton = document.querySelector("#start");
-var quizQuestion = document.createElement("ul");
-var timer = document.getElementById("countdown")
+var timer = document.getElementById("#countdown")
+var a1 = document.getElementById('#answer1');
+var a2 = document.getElementById('#answer2');
+var a3 = document.getElementById('#answer3');
+var a4 = document.getElementById('#answer4');
 var questionPlacement = document.createElement('h1');
-var mainEl = document.getElementById('main');
-questionPlacement.textContent = "question";
+var a1Placement = document.createElement('h2')
+var onOff = 0;
+
+//questionPlacement.textContent = "question";
+var questionCounter = 0;
 
 //setInterval( test, 50000);
 
 startButton.addEventListener("click", function() {
-    timer.textContent = 60;
+    if(onOff == 0)
+    {
+    timer.textContent = 10; // make it 60;
     timerForQuestions();
+    questions();
     document.body.appendChild(questionPlacement);
-
+    }
+    onOff++;
   });
 
 
   function questions()
   {
-    //setInterval( test, 50000); //timer
-    //use the thing where you can add stuff to the page and post the question and the timer
-    startButton.innerHTML = "CLICK ME";
+    if(questionCounter == 0)
+    {
+        questionPlacement.textContent = "Question 1: question";
+        a1.textContent = "answer 1";
+        document.body.appendChild(a1Placement);
+        //a2.textContent = "answer 2";
+        //a3.textContent = "answer 3";
+        //a4.textContent = "answer 4";
+    }
+    if(questionCounter == 1)
+    {
+        questionPlacement.textContent = "Question 2: question";
+        a1.textContent = "answer 11";
+        a2.textContent = "answer 22";
+        a3.textContent = "answer 33";
+        a4.textContent = "answer 44";
+    }
+    if(questionCounter == 2)
+    {
+        questionPlacement.textContent = "Question 3: question";
+        a1.textContent = "answer 11";
+        a2.textContent = "answer 22";
+        a3.textContent = "answer 33";
+        a4.textContent = "answer 44";
+    }
+    if(questionCounter == 3)
+    {
+        questionPlacement.textContent = "Question 4: question";
+        a1.textContent = "answer 11";
+        a2.textContent = "answer 22";
+        a3.textContent = "answer 33";
+        a4.textContent = "answer 44";
+    }
+    if(questionCounter == 4)
+    {
+        questionPlacement.textContent = "Question 5: question";
+        a1.textContent = "answer 11";
+        a2.textContent = "answer 22";
+        a3.textContent = "answer 33";
+        a4.textContent = "answer 44";
+    }
+
+    
+
+
+    questionCounter++;
   }
 
 
