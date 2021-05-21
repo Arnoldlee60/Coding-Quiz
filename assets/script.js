699459a3fd7a6ff92b3cbb2fe1bcf7c192354c1b
Arnold Lee <arnoldlee60@gmail.com>
2021-05-21 00:46:46 -0500

    got to a point where i can stop and start tomarrow

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,18 @@
 var startButton = document.querySelector("#start");
+//setInterval( test, 50000);
 
 startButton.addEventListener("click", function() {
-  
-    
-
-    
+    console.log("asd")
   });
+
+  function test(){
+      alert("it worked")
+  }
+
+  function questions()
+  {
+    setInterval( test, 50000); //timer
+    //use the thing where you can add stuff to the page and post the question and the timer
+
+  }
+
