8c9c9c3fbeb111c73445a163b78078a0910fc277
Arnold Lee <arnoldlee60@gmail.com>
2021-05-21 13:44:11 -0500

    timer and set up for question is set up

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,35 @@
 var startButton = document.querySelector("#start");
+var quizQuestion = document.createElement("ul");
+var timer = document.getElementById("countdown")
+var questionPlacement = document.createElement('h1');
+var mainEl = document.getElementById('main');
+questionPlacement.textContent = "question";
+
 //setInterval( test, 50000);
 
 startButton.addEventListener("click", function() {
-    console.log("asd")
+    timer.textContent = 60;
+    timerForQuestions();
+    document.body.appendChild(questionPlacement);
+
   });
 
-  function test(){
-      alert("it worked")
-  }
 
   function questions()
   {
-    setInterval( test, 50000); //timer
+    //setInterval( test, 50000); //timer
     //use the thing where you can add stuff to the page and post the question and the timer
-
+    startButton.innerHTML = "CLICK ME";
   }
+
+
+  function timerForQuestions(){
+    var seconds = setInterval(function() { //countdown every second
+
+            if (timer.textContent != 0)
+            {
+                timer.textContent--; 
+            }
+        }, 1000)
+}
 
