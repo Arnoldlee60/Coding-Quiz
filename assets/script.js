73aafbaea6bf372d0bad31a8f59253847bf6fced
Arnold Lee <arnoldlee60@gmail.com>
2021-05-21 21:48:42 -0500

    added in questions and answers

--- assets/script.js
+++ assets/script.js
@@ -39,17 +39,17 @@ startButton.addEventListener("click", function() {
   {
     if(questionCounter == 0)
     {
-        questionPlacement.textContent = "Question 1: question";
-        a1Placement.textContent = "answer 1";
+        questionPlacement.textContent = "Question 1: What is 2 + 2 = ?";
+        a1Placement.textContent = "1.   four";
             a1.innerHTML = "1";
             document.body.appendChild(a1);
-        a2Placement.textContent = "answer 2";
+        a2Placement.textContent = "2.   five";
         a2.innerHTML = "2";
             document.body.appendChild(a2);
-        a3Placement.textContent = "answer 3";
+        a3Placement.textContent = "3.   one";
             a3.innerHTML = "3";
             document.body.appendChild(a3);
-        a4Placement.textContent = "answer 4";
+        a4Placement.textContent = "4.   fish";
             a4.innerHTML = "4";
             document.body.appendChild(a4);
         
@@ -73,17 +73,17 @@ startButton.addEventListener("click", function() {
     }
     if(questionCounter == 1)
     {
-        questionPlacement.textContent = "Question 2: question";
-        a1Placement.textContent = "answer 1";
+        questionPlacement.textContent = "Question 2: What is a dog?";
+        a1Placement.textContent = "1.   A reptile";
             a1.innerHTML = "1";
             document.body.appendChild(a1);
-        a2Placement.textContent = "answer 2";
+        a2Placement.textContent = "2.   a mammal";
         a2.innerHTML = "2";
             document.body.appendChild(a2);
-        a3Placement.textContent = "answer 3";
+        a3Placement.textContent = "3.   a bird";
             a3.innerHTML = "3";
             document.body.appendChild(a3);
-        a4Placement.textContent = "answer 4";
+        a4Placement.textContent = "4.   a fish";
             a4.innerHTML = "4";
             document.body.appendChild(a4);
         
@@ -107,17 +107,17 @@ startButton.addEventListener("click", function() {
     }
     if(questionCounter == 2)
     {
-        questionPlacement.textContent = "Question 3: question";
-        a1Placement.textContent = "answer 1";
+        questionPlacement.textContent = "Question 3: Which food does not spoil ";
+        a1Placement.textContent = "1.   eggs";
             a1.innerHTML = "1";
             document.body.appendChild(a1);
-        a2Placement.textContent = "answer 2";
+        a2Placement.textContent = "2.   milk";
         a2.innerHTML = "2";
             document.body.appendChild(a2);
-        a3Placement.textContent = "answer 3";
+        a3Placement.textContent = "3.   honey";
             a3.innerHTML = "3";
             document.body.appendChild(a3);
-        a4Placement.textContent = "answer 4";
+        a4Placement.textContent = "4.   figs";
             a4.innerHTML = "4";
             document.body.appendChild(a4);
         
@@ -141,17 +141,17 @@ startButton.addEventListener("click", function() {
     }
     if(questionCounter == 3)
     {
-        questionPlacement.textContent = "Question 4: question";
-        a1Placement.textContent = "answer 1";
+        questionPlacement.textContent = "Question 4: what color is blood in your body";
+        a1Placement.textContent = "1.   blue";
             a1.innerHTML = "1";
             document.body.appendChild(a1);
-        a2Placement.textContent = "answer 2";
+        a2Placement.textContent = "2.   purple";
         a2.innerHTML = "2";
             document.body.appendChild(a2);
-        a3Placement.textContent = "answer 3";
+        a3Placement.textContent = "3.   green";
             a3.innerHTML = "3";
             document.body.appendChild(a3);
-        a4Placement.textContent = "answer 4";
+        a4Placement.textContent = "4.   red";
             a4.innerHTML = "4";
             document.body.appendChild(a4);
         
@@ -175,17 +175,17 @@ startButton.addEventListener("click", function() {
     }
     if(questionCounter == 4)
     {
-        questionPlacement.textContent = "Question 5: question";
-        a1Placement.textContent = "answer 1";
+        questionPlacement.textContent = "Question 5: Fastest bird in the world";
+        a1Placement.textContent = "Peregrine falcon";
             a1.innerHTML = "1";
             document.body.appendChild(a1);
-        a2Placement.textContent = "answer 2";
+        a2Placement.textContent = "Hummingbird";
         a2.innerHTML = "2";
             document.body.appendChild(a2);
-        a3Placement.textContent = "answer 3";
+        a3Placement.textContent = "Raven";
             a3.innerHTML = "3";
             document.body.appendChild(a3);
-        a4Placement.textContent = "answer 4";
+        a4Placement.textContent = "Bald eagle";
             a4.innerHTML = "4";
             document.body.appendChild(a4);
         
