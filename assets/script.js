4bc59dfb25737c986f00995cb7b4a367ccfec3b9
Arnold Lee <arnoldlee60@gmail.com>
2021-05-21 20:14:56 -0500

    clicking the answer brings up the wrong question

--- assets/script.js
+++ assets/script.js
@@ -16,20 +16,23 @@ var questionCounter = 0;
 
 
 startButton.addEventListener("click", function() {
+    timerForQuestions(); //if you click multiple times the time goes faster Im not really sure what to do
+    begin();
+  });
+
+  function begin(){
     if(onOff == 0)
     {
     timer.textContent = 60; // make it 60;
-    timerForQuestions();
     questions();
     document.body.appendChild(questionPlacement);
         document.body.appendChild(a1Placement);
         document.body.appendChild(a2Placement);
         document.body.appendChild(a3Placement);
         document.body.appendChild(a4Placement);
-        
     }
-    onOff++;
-  });
+    onOff++; 
+  }
 
 
   function questions()
@@ -52,6 +55,8 @@ startButton.addEventListener("click", function() {
         
         a1.addEventListener("click", function() {
         correct++;
+        onOff = 0;
+        begin();
         });
         a2.addEventListener("click", function() {
             incorrect++;
@@ -69,35 +74,143 @@ startButton.addEventListener("click", function() {
     if(questionCounter == 1)
     {
         questionPlacement.textContent = "Question 2: question";
-        a1Placement.textContent = "cat";
-        a2Placement.textContent = "dog";
-        a3Placement.textContent = "fish";
-        a4Placement.textContent = "bird";
+        a1Placement.textContent = "answer 1";
+            a1.innerHTML = "1";
+            document.body.appendChild(a1);
+        a2Placement.textContent = "answer 2";
+        a2.innerHTML = "2";
+            document.body.appendChild(a2);
+        a3Placement.textContent = "answer 3";
+            a3.innerHTML = "3";
+            document.body.appendChild(a3);
+        a4Placement.textContent = "answer 4";
+            a4.innerHTML = "4";
+            document.body.appendChild(a4);
+        
+        a1.addEventListener("click", function() {
+        incorrect++;
+        timer.textContent = timer.textContent - 5; 
+        });
+        a2.addEventListener("click", function() {
+            correct++;
+            onOff = 0;
+            begin();
+        });
+        a3.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
+        a4.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
     }
     if(questionCounter == 2)
     {
         questionPlacement.textContent = "Question 3: question";
-        a1Placement.textContent = "yummy";
-        a2Placement.textContent = "atasf";
-        a3Placement.textContent = "answasfagwever 3";
-        a4Placement.textContent = "answwadaer 4";
+        a1Placement.textContent = "answer 1";
+            a1.innerHTML = "1";
+            document.body.appendChild(a1);
+        a2Placement.textContent = "answer 2";
+        a2.innerHTML = "2";
+            document.body.appendChild(a2);
+        a3Placement.textContent = "answer 3";
+            a3.innerHTML = "3";
+            document.body.appendChild(a3);
+        a4Placement.textContent = "answer 4";
+            a4.innerHTML = "4";
+            document.body.appendChild(a4);
+        
+        a1.addEventListener("click", function() {
+        incorrect++;
+        timer.textContent = timer.textContent - 5; 
+        });
+        a2.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
+        a3.addEventListener("click", function() {
+            correct++;
+            onOff = 0;
+            begin();
+        });
+        a4.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
     }
     if(questionCounter == 3)
     {
         questionPlacement.textContent = "Question 4: question";
-        a1Placement.textContent = "ansvawvawwer 1";
-        a2Placement.textContent = "ansvfghytfwer 2";
-        a3Placement.textContent = "answdfghrsefder 3";
-        a4Placement.textContent = "answghwe345er 4";
+        a1Placement.textContent = "answer 1";
+            a1.innerHTML = "1";
+            document.body.appendChild(a1);
+        a2Placement.textContent = "answer 2";
+        a2.innerHTML = "2";
+            document.body.appendChild(a2);
+        a3Placement.textContent = "answer 3";
+            a3.innerHTML = "3";
+            document.body.appendChild(a3);
+        a4Placement.textContent = "answer 4";
+            a4.innerHTML = "4";
+            document.body.appendChild(a4);
+        
+        a1.addEventListener("click", function() {
+        incorrect++;
+        timer.textContent = timer.textContent - 5; 
+        });
+        a2.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
+        a3.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
+        a4.addEventListener("click", function() {
+            correct++;
+            onOff = 0;
+            begin();
+        });
     }
-    if(questionCounter == 4)
+    if(questionCounter == 1)
     {
         questionPlacement.textContent = "Question 5: question";
-        a1Placement.textContent = "answer1erefgrt3e 1";
-        a2Placement.textContent = "answefqgbrger 2";
-        a3Placement.textContent = "answergrbwefv 3";
-        a4Placement.textContent = "answeevbgrer 4";
+        a1Placement.textContent = "answer 1";
+            a1.innerHTML = "1";
+            document.body.appendChild(a1);
+        a2Placement.textContent = "answer 2";
+        a2.innerHTML = "2";
+            document.body.appendChild(a2);
+        a3Placement.textContent = "answer 3";
+            a3.innerHTML = "3";
+            document.body.appendChild(a3);
+        a4Placement.textContent = "answer 4";
+            a4.innerHTML = "4";
+            document.body.appendChild(a4);
+        
+        a1.addEventListener("click", function() {
+        incorrect++;
+        timer.textContent = timer.textContent - 5; 
+            correct++;
+            onOff = 0;
+            begin();
+        });
+        a2.addEventListener("click", function() {
+            incorrect++;
+        timer.textContent = timer.textContent - 5; 
+        });
+        a3.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
+        a4.addEventListener("click", function() {
+            incorrect++;
+            timer.textContent = timer.textContent - 5; 
+        });
     }
+    
+    
     questionCounter++;
   }
 
